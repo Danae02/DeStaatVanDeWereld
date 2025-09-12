f678d5e17945fa0c0ec9cc93907bba610fd84ded
Danae02 <danae.vandermeer@student.hu.nl>
2025-09-12 21:00:49 +0200

    css werkt?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/DeStaatVanDeWereld/'  // let op hoofdletters!
+    base: '/DeStaatVanDeWereld/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                about: resolve(__dirname, 'src/about.html')
+            }
+        }
+    }
 });
